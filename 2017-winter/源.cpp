b1b1d2856bac17fffaadba46d73b25551a322585
Tizen Ye <yzs981130@126.com>
2017-01-22 12:12:49 +0800

    hihocoder 3 KMP

--- 2017-winter/源.cpp
+++ 2017-winter/源.cpp
@@ -3,61 +3,56 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-struct Node
+#define MAXN 1000100
+int Next[MAXN];
+void makeNext(const string &s)
 {
-	int cnt;
-	Node *ch[26];
-	Node() { memset(ch, 0, sizeof(ch)); cnt = 0; }
-}*root;
-void Insert(const string &str)
-{
-	Node *current, *newnode;
-	int len = str.length();
-	current = root;
-	for (int i = 0; i < len; i++)
-		if (current->ch[str[i] - 'a'] != NULL)
+	int i = 0, j = -1, len = s.length();
+	while(i < len)
+	{
+		if (j == -1 || s[i] == s[j])
 		{
-			current = current->ch[str[i] - 'a'];
-			current->cnt++;
+			Next[i] = j;
+			i++, j++;
 		}
 		else
-		{
-			newnode = new Node();
-			current->ch[str[i] - 'a'] = newnode;
-			current = current->ch[str[i] - 'a'];
-			current->cnt = 1;
-		}
+			j = Next[j];
+	}
 }
-int Find(const string &str)
+int KMP(const string &p, const string &q)
 {
-	Node *current;
-	int len = str.length();
-	if (len == 0)
-		return 0;
-	current = root;
-	for (int i = 0; i < len; i++)
-		if (current->ch[str[i] - 'a'] != NULL)
-			current = current->ch[str[i] - 'a'];
-		else
-			return 0;
-	return current->cnt;
+	int cnt = 0, lenp = p.length(), lenq = q.length();
+	makeNext(q);
+	for(int i = 0, j = 0; i < lenp; i++)
+	{
+		while(i < lenp && j < lenq)
+		{
+			if (p[i] == q[j])
+				i++, j++;
+			else if (j == 0)
+				i++;
+			else
+				j = Next[j - 1] + 1;
+		}
+		if(j == lenq)
+		{
+			cnt++;
+			j = Next[j - 1] + 1;
+			i += j - lenq;
+		}
+	}
+	return cnt;
 }
 int main()
 {
-	int cntd = 0, cntq = 0;
-	cin >> cntd;
-	string s;
-	root = new Node();
-	for(int i = 0; i < cntd; i++)
+	int n;
+	cin >> n;
+	string s1, s2;
+	while(n--)
 	{
-		cin >> s;
-		Insert(s);
-	}
-	cin >> cntq;
-	for(int i = 0; i < cntq; i++)
-	{
-		cin >> s;
-		cout << Find(s) << endl;
+		memset(Next, 0, sizeof(Next));
+		cin >> s1 >> s2;
+		cout << KMP(s2, s1) << endl;
 	}
 	system("pause");
 	return 0;
